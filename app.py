7db410bcc79f85446b67e42880bb7453a27018da
rjanke <jankersr@yahoo.com>
2018-08-05 18:33:20 -0600

    Added more comments and added a year field.

--- app.py
+++ app.py
@@ -4,10 +4,12 @@ from form import BookForm
 app = Flask(__name__)
 
 # Set secret for CSRF protection from Flask WTF to work.
-app.secret_key = "hithisisbob"
+app.secret_key = "thisissupersecret"
 
 @app.route("/", methods=('GET', 'POST'))
 def book():
+
+    # Set BookForm so we can send it's data to the template for rendering.
     form = BookForm()
 
     if form.validate_on_submit():
@@ -18,16 +20,18 @@ def book():
         session['last_name'] = form.last_name.data
         session['publisher'] = form.publisher.data
         session['book_title'] = form.book_title.data
+        session['year'] = form.year.data
         # Reset the form field to blank.
         form.first_name.data = ''
         form.last_name.data = ''
         form.publisher.data = ''
         form.book_title.data = ''
+        form.year.data = ''
         # redirect after form submit.
         return redirect(url_for('book'))
 
     # Set the key we will be testing for.
-    key = 'first_name'
+    key = 'year'
 
     # See if the key exists already, if it does, then send the session data
     # to the template.
@@ -37,13 +41,14 @@ def book():
                                 last_name=session['last_name'], 
                                 publisher=session['publisher'],
                                 book_title=session['book_title'],
+                                year=session['year'],
                                 form=form)
     
     # If it doesn't exist, render the template without sending the session data.
     else:
         return render_template("book.html", form=form)
 
-
+# This is an example of another route for translated books.
 @app.route('/trans-book')
 def trans_book():
     return render_template('trans_book.html')
--- form.py
+++ form.py
@@ -2,8 +2,12 @@ from flask_wtf import FlaskForm
 from wtforms import StringField
 from wtforms.validators import DataRequired
 
+# Create our form by extending FlaskForm.
 class BookForm(FlaskForm):
+    # Set field types to a variable name. Choose validators needed.
+    # Refer to WTForms docs for more Field types and validators.
     first_name = StringField('First name', validators=[DataRequired()])
     last_name = StringField('Last name', validators=[DataRequired()])
     publisher = StringField('Publisher', validators=[DataRequired()])
     book_title = StringField('Book title', validators=[DataRequired()])
+    year = StringField('Year', validators=[DataRequired()])
